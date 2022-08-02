e6a94803c23b444fefc1b2ba172e470a43fedf32
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-08-02 23:58:38 +0200

    Updated script for testing

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
+# [Enviroment/Tools]
+
 export FLASK_APP=app
 export FLASK_ENV=development
 export FLASK_DEBUG=1
 
+# [Mail]
+export APP_ADMIN='fela55555@wp.pl'
+
+# [Python venv]
 source ./env/bin/activate
 
+# [Flas]
 flask run
