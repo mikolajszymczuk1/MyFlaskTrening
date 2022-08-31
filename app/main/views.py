f914827e7ee3fc812c989ec92eba1b93f815c99a
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-09-01 00:37:58 +0200

    Render all posts on profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, abort
 from . import main
 from .. import db
 from ..models import Permission, User, Post
@@ -25,7 +25,11 @@ def index():
 @main.get('/user/<string:username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    if user is None:
+        abort(404)
+
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
